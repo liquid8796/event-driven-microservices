affce7cabf2af2d2656df344543e5d6ca2792e8b
SACOMBANK\os-namtlh0807962 <os-namtlh0807962@sacombank.com>
2025-07-30 08:42:02 +0000

    2025-07-30 15:42:02 +07:00 Asia/Saigon

--- section1/01_end/gatewayserver/src/main/java/com/eazybytes/gatewayserver/router/CustomerCompositeRouter.java
+++ section1/01_end/gatewayserver/src/main/java/com/eazybytes/gatewayserver/router/CustomerCompositeRouter.java
@@ -3,7 +3,10 @@ package com.eazybytes.gatewayserver.router;
 import com.eazybytes.gatewayserver.handler.CustomerCompositeHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RequestPredicates;
 import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
 @Configuration(proxyBeanMethods = false)
@@ -11,6 +14,9 @@ public class CustomerCompositeRouter {
 
     @Bean
     public RouterFunction<ServerResponse> route(CustomerCompositeHandler customerCompositeHandler) {
-        return null;
+        return RouterFunctions.route(RequestPredicates.GET("/api/composite/fetchCustomerSummary")
+                        .and(RequestPredicates.accept(MediaType.APPLICATION_JSON))
+                        .and(RequestPredicates.queryParam("mobileNumber", )),
+                customerCompositeHandler::fetchCustomerSummary);
     }
 }

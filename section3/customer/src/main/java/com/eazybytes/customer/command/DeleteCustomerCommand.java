f3fc71c02db240c8f4e5cbcbe952a96fa5b4ed54
SACOMBANK\os-namtlh0807962 <os-namtlh0807962@sacombank.com>
2025-08-07 10:06:42 +0000

    2025-08-07 17:06:42 +07:00 Asia/Saigon

---
+++ section3/customer/src/main/java/com/eazybytes/customer/command/DeleteCustomerCommand.java
@@ -0,0 +1,17 @@
+package com.eazybytes.customer.command;
+
+import lombok.Builder;
+import lombok.Data;
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
+
+/**
+ * VERB+NOUN+Command
+ */
+@Data
+@Builder
+public class DeleteCustomerCommand {
+
+    @TargetAggregateIdentifier
+    private final String customerId;
+    private final boolean activeSw;
+}

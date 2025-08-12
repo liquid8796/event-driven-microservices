d1a410532a917099b789519aeb85e15a9dbbb271
liquid8796 <hanam.tranle.5@gmail.com>
2025-08-12 15:51:53 +0000

    2025-08-12 22:51:53 +07:00 Asia/Saigon

--- section3/customer/src/main/java/com/eazybytes/customer/command/aggregate/CustomerAggregate.java
+++ section3/customer/src/main/java/com/eazybytes/customer/command/aggregate/CustomerAggregate.java
@@ -1,11 +1,15 @@
 package com.eazybytes.customer.command.aggregate;
 
 import com.eazybytes.customer.command.CreateCustomerCommand;
+import com.eazybytes.customer.command.event.CustomerCreatedEvent;
 import com.eazybytes.customer.entity.Customer;
+import com.eazybytes.customer.exception.CustomerAlreadyExistsException;
 import com.eazybytes.customer.repository.CustomerRepository;
 import org.axonframework.commandhandling.CommandHandler;
 import org.axonframework.modelling.command.AggregateIdentifier;
+import org.axonframework.modelling.command.AggregateLifecycle;
 import org.axonframework.spring.stereotype.Aggregate;
+import org.springframework.beans.BeanUtils;
 
 import java.util.Optional;
 
@@ -23,5 +27,13 @@ public class CustomerAggregate {
     @CommandHandler
     public CustomerAggregate(CreateCustomerCommand createCustomerCommand, CustomerRepository customerRepository) {
         Optional<Customer> optionalCustomer = customerRepository.findByMobileNumberAndActiveSw(createCustomerCommand.getMobileNumber(), true);
+        if (optionalCustomer.isPresent()) {
+            throw new CustomerAlreadyExistsException("Customer already registered with given mobileNumber "
+                    + createCustomerCommand.getMobileNumber());
+        }
+
+        CustomerCreatedEvent customerCreatedEvent = new CustomerCreatedEvent();
+        BeanUtils.copyProperties(createCustomerCommand, customerCreatedEvent);
+        AggregateLifecycle.apply(customerCreatedEvent);
     }
 }

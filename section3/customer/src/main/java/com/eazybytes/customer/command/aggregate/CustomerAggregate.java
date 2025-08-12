cb40faee23aeebc4723f42d28a9edb7025bb7bb9
liquid8796 <hanam.tranle.5@gmail.com>
2025-08-12 15:34:17 +0000

    2025-08-12 22:34:17 +07:00 Asia/Saigon

---
+++ section3/customer/src/main/java/com/eazybytes/customer/command/aggregate/CustomerAggregate.java
@@ -0,0 +1,24 @@
+package com.eazybytes.customer.command.aggregate;
+
+import com.eazybytes.customer.command.CreateCustomerCommand;
+import com.eazybytes.customer.repository.CustomerRepository;
+import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.modelling.command.AggregateIdentifier;
+import org.axonframework.spring.stereotype.Aggregate;
+
+@Aggregate
+public class CustomerAggregate {
+    @AggregateIdentifier
+    private String customerId;
+    private String name;
+    private String email;
+    private String mobileNumber;
+    private boolean activeSw;
+
+    public CustomerAggregate() {}
+
+    @CommandHandler
+    public CustomerAggregate(CreateCustomerCommand createCustomerCommand, CustomerRepository customerRepository) {
+
+    }
+}

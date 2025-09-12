59f04ee197b01cafda283e8fc19d5558a7df15c0
SACOMBANK\os-namtlh0807962 <os-namtlh0807962@sacombank.com>
2025-09-12 02:04:10 +0000

    2025-09-12 09:04:10 +07:00 Asia/Saigon

--- section3/customer/src/main/java/com/eazybytes/customer/command/aggregate/CustomerAggregate.java
+++ section3/customer/src/main/java/com/eazybytes/customer/command/aggregate/CustomerAggregate.java
@@ -41,7 +41,7 @@ public class CustomerAggregate {
                 findByMobileNumberAndActiveSw(createCustomerCommand.getMobileNumber(), true);
         if (optionalCustomer.isPresent()) {
             throw new CustomerAlreadyExistsException("Customer already registered with given mobileNumber "
-                    + createCustomerCommand.getMobileNumber());e
+                    + createCustomerCommand.getMobileNumber());
         }*/
         CustomerCreatedEvent customerCreatedEvent = new CustomerCreatedEvent();
         BeanUtils.copyProperties(createCustomerCommand, customerCreatedEvent);

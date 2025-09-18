c355921380b8d17d6153e07c3ec1df2958b0a282
SACOMBANK\os-namtlh0807962 <os-namtlh0807962@sacombank.com>
2025-09-18 08:01:48 +0000

    2025-09-18 15:01:48 +07:00 Asia/Saigon

--- section4/eazy-bom/common/src/main/java/com/eazybytes/common/event/LoanDataChangedEvent.java
+++ section4/eazy-bom/common/src/main/java/com/eazybytes/common/event/LoanDataChangedEvent.java
@@ -3,9 +3,8 @@ package com.eazybytes.common.event;
 import lombok.Data;
 
 @Data
-public class CustomerDataChangedEvent {
+public class LoanDataChangedEvent {
 
-    private String name;
     private String mobileNumber;
-    private boolean activeSw;
+    private Long loanNumber;
 }
